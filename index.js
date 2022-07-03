dd424162afd6bb22916ca459e82f4b99f6979f34
matthewelam <ematew418@gmail.com>
2022-07-03 09:27:15 -0400

    commit tree 34ef700b9748e024f1c3e903a4a622b1ce6dbf2f

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+const employee = {
+    name: "Matt",
+    streetAddress: "4919 Clintwood Lane",
+  };
+
+
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+const newEmployee = {...employee};
+newEmployee[key] = value;
+return newEmployee;
+}
+
+function  destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return Employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const newObject = Object.assign({}, employee)
+    delete newObject[key]
+    return newObject;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    const newObject = Object.assign(employee, key)
+    newObject[employee] = '';
+    return newObject[key];
+}
